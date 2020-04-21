abd89596cc3830a2bae29a694c017a07ae46f874
york zhang <york.zhang@omnyfy.com>
2020-04-22 02:04:58 +0800

    simplest sample

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { t, i18n } = useTranslation();
+
+    const onChangeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    };
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <div>
+                    <button onClick={() => onChangeLanguage('en')}>
+                        English
+                    </button>
+                    <button onClick={() => onChangeLanguage('cn')}>中文</button>
+                </div>
+                <p>{t('Welcome to React')}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
